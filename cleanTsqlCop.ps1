26d69607f223d5b63addc9f4ebb4a8295927bf5b
Todd Bennett <tebennett@eprod.com>
2019-12-09 08:44:18 -0600

    Added comments

--- cleanTsqlCop.ps1
+++ cleanTsqlCop.ps1
@@ -1,21 +1,25 @@
-﻿$name = (dir ~\AppData\Local\Microsoft\VisualStudio | Where-Object {$_.Name -like '15.0*'} | Select-Object -Property Name).Name
+﻿# load Visual Studio's private registry hive
+$name = (dir ~\AppData\Local\Microsoft\VisualStudio | Where-Object {$_.Name -like '15.0*'} | Select-Object -Property Name).Name
 pushd "~\AppData\Local\Microsoft\VisualStudio\$name"
 copy .\privateregistry.bin .\privateregistry.backup.bin
 reg load HKLM\isolatedHive .\privateregistry.bin
 pushd "HKLM:\isolatedHive\Software\Microsoft\VisualStudio\${name}_Config\"
 
+# remove TSqlCop's extension package by name
 pushd '.\InstalledProducts\'
 if (Test-Path -Path '.\SqlPackage') {
     Remove-Item 'SqlPackage'
 }
 popd
 
+# remove TSqlCop's package by GUID
 pushd '.\Packages\'
 if (Test-Path -Path '.\{B94020EE-B348-4105-A362-A2ED89388CC8}') {
     Remove-Item '.\{B94020EE-B348-4105-A362-A2ED89388CC8}'
 }
 popd
 
+# remove TSqlCop's autoload GUIDs
 pushd '.\AutoLoadPackages\'
 if (Test-Path -Path '.\{adfc4e64-0397-11d1-9f4e-00a0c911004f}') {
     Remove-Item '.\{adfc4e64-0397-11d1-9f4e-00a0c911004f}'
@@ -25,11 +29,15 @@ if (Test-Path -Path '.\{f1536ef8-92ec-443c-9ed7-fdadf150da82}') {
 }
 popd
 
+# remove TSqlCop's menus
 Remove-ItemProperty -Path '.\Menus' -Name '{B94020EE-B348-4105-A362-A2ED89388CC8}' -ErrorAction SilentlyContinue
 
+# force the garbage collector to release any references to registry entries immediately
 popd
 [gc]::Collect()
 [gc]::WaitForPendingFinalizers()
+
+# unload hive
 reg unload HKLM\isolatedHive
 popd
 popd
